1c0113680b93526876595d40c28766bbfb799ee0
Franken Stoned <k.elzner@gmail.com>
2026-06-06 17:56:00 +0000

    File changes

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -271,7 +271,7 @@ export default function Gallery() {
             </button>
           </div> :
 
-        <div className="grid grid-cols-2 gap-3">
+        <div className="grid grid-cols-3 gap-2">
             {clips.map((clip, i) =>
           <motion.div
             key={clip.id}
