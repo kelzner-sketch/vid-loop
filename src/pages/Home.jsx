857d14fecec8b00703caf00aaae9a9cc5bed6172
Franken Stoned <k.elzner@gmail.com>
2026-06-06 15:01:25 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -261,7 +261,7 @@ export default function Home() {
             {/* Logo mark */}
             <div className="relative">
               <div className="w-24 h-24 rounded-3xl border border-primary/20 flex items-center justify-center opacity-100 bg-[#184291]">
-                <Camera className="w-10 h-10 text-primary" />
+                <Camera className="w-20 h-20 text-primary" />
               </div>
               <div className="absolute -top-1 -right-1 w-4 h-4 rounded-full bg-sky-300 animate-pulse" />
             </div>
